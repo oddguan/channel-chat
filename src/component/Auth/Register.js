7e11e1d405c276050216f0433fb08441a514cbd1
Chenxiao Guan <1011zaozao@gmail.com>
2019-02-22 18:27:28 -0500

    finished register functionalities

--- src/component/Auth/Register.js
+++ src/component/Auth/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import firebase from '../../firebase';
+import md5 from 'md5';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ class Register extends React.Component {
     passwordConfirmation: '',
     errors: [],
     loading: false,
+    usersRef: firebase.database().ref('users'),
   };
 
   isFormValid = () => {
@@ -52,23 +54,41 @@ class Register extends React.Component {
   };
 
   handleSubmit = event => {
-    console.log('handle submit');
     event.preventDefault(); /* Default is to reload the page, we want to prevent that. */
     if (this.isFormValid()) {
-      console.log('form valid');
       this.setState({ errors: [], loading: true })
       firebase
         .auth()
         .createUserWithEmailAndPassword(this.state.email, this.state.password)
         .then(createdUser => {
           console.log(createdUser);
-          this.setState({ loading: false });
+          createdUser.user.updateProfile({
+            displayName: this.state.username,
+            photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=indenticon`,
+          })
+          .then(() => {
+            // this.setState({ loading: false });
+            this.saveUser(createdUser).then(() => {
+              console.log('user saved');
+            })
+          })
+          .catch((err) => {
+            console.error(err);
+            this.setState({ errors: this.state.errors.concat(err), loading: false });
+          })
         })
         .catch(err => {
           console.error(err);
           this.setState({ errors: this.state.errors.concat(err), loading: false });
         });
     }
+  };
+
+  saveUser = createdUser => {
+    return this.state.usersRef.child(createdUser.user.uid).set({
+      name: createdUser.user.displayName,
+      avatar: createdUser.user.photoURL,
+    });
   };
 
   handleInputError = (errors, inputName) => {
