97b80d900f12b0f3ca27e4016b9a82d3b985e53f
Chenxiao Guan <1011zaozao@gmail.com>
2019-02-23 21:56:39 -0500

    created a dropdown for loggedin user

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './component/App';
-import Login from './component/Auth/Login';
-import Register from './component/Auth/Register';
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import Spinner from './Spinner';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from './firebase';
 
@@ -34,7 +35,7 @@ class Root extends React.Component {
   }
 
   render() {
-    return (
+    return this.props.isLoading ? <Spinner /> : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
@@ -44,9 +45,14 @@ class Root extends React.Component {
   }
 }
 
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading,
+});
+
+
 const RootWithAuth = withRouter(
   connect(
-    null,
+    mapStateFromProps,
     { setUser }
   )(Root)
 ); // a higher order component to wrap the the root component
